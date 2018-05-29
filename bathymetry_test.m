9cd4e130eae9dfe97362a8cd9cc5bb396a660c15
kpdreamer <sunkpfly@126.com>
2018-05-29 15:15:27 +0800

    bathymetry_test

---
+++ bathymetry_test.m
@@ -0,0 +1,10 @@
+clc;clear;close all
+%%
+m_proj('Mercator','lon',[117 123],'lat',[36 42])
+% m_etopo2('contourf',-100:1:0,'edgecolor','none');
+[elevations,lat,lon]=mygrid_sand2([117 123 36 42]);
+  m_contour(lon,lat,elevations);
+m_gshhs_l('patch',[.5 .8 0],'edgecolor','none');
+m_grid('linewi',2,'layer','top');
+caxis([-40 0])
+colormap(m_colmap('blue')); 
