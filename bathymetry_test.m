28af04d9a8bc8f586773521444c95cae4f412db2
kpdreamer <sunkpfly@126.com>
2018-07-09 10:28:10 +0800

    test

--- bathymetry_test.m
+++ bathymetry_test.m
@@ -5,8 +5,8 @@ m_proj('Mercator','lon',[117 129],'lat',[24 42])
 % m_etopo2('contourf',-7000:20:0,'edgecolor','none');
 [elevations,lon,lat]=m_etopo2([117 129 24 42]);
 elevations(elevations >= 0) = 0;
-elevations(elevations == 0) = -realmin();
-m_contour(lon,lat,-log(-elevations));
+elevations(elevations == 0) = nan;
+m_contourf(lon,lat,-log(-elevations));
 m_gshhs_i('patch',[96/255 96/255 96/255],'edgecolor','k');
 m_grid('linewi',2,'layer','top');
 % caxis([-1000 50])
